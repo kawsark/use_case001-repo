5bf99b9effe6781d49ad607e4175353e0f016f67
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2018-11-16 08:15:57 -0600

    Create main.tf

---
+++ prod/main.tf
@@ -0,0 +1,39 @@
+data "terraform_remote_state" "network" {
+  backend = "atlas"
+
+  config {
+    name = "${var.org}/${var.workspace_name}"
+  }
+}
+
+provider "aws" {
+  region = "${data.terraform_remote_state.network.region}"  
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  subnet_id = "${data.terraform_remote_state.network.prod_subnet_id}"
+
+  tags {
+    Name = "Use_Case002-Prod-Instance"
+    owner = "vinnie@hashicorp.com"
+    TTL = "1"
+  }
+}
